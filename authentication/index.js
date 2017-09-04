96ce5664b0d4310e82e5fff67c466149d8efe876
Rick Gallegos <duulketariakan@yahoo.com>
2017-09-03 18:50:19 -0700

    Ready for DB

--- authentication/index.js
+++ authentication/index.js
@@ -1,4 +1,28 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google').Strategy;
 
-//add database here for saving usernames and contacts
+////add database here for saving usernames and contacts
+//const db = require('../db');
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL,
+  }
+))
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  const userInfo = {
+    displayName: user.username,
+    email: user.email,
+    avatarUrl: user.avatarUrl
+  }
+  done(null, userInfo);
+});
+
+exports.passport = passport;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var app = express();
+//library for authentication
+const session = require('express-session');
+// const passport = require('../authentication')
+
+const app = express();
 app.use(bodyParser.urlencoded( {extended: true }));
 app.use(bodyParser.json());
+
+
+//Save sessions
+// app.use(session({
+//   secret: 'keep safe yo',
+//   resave: false,
+//   saveUninitialized: true,
+// }));
+//
+// //Set server to use initialized passport from authentication module
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 /////////////////////////////////////////////////////////////
 /////////////////////// GET REQUESTS ///////////////////////

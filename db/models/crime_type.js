7f6f3e1abf909b66ff1e26594e70d3cf65b6d8a2
Sonrisa <sonrisa722611@gmail.com>
2017-09-05 22:28:24 -0700

    Update gitignore

--- db/models/crime_type.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  const crime_type = sequelize.define('crimetype', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    type: DataTypes.STRING,
-  });
-  return crime_type;
-}

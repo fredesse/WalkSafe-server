7fd376628121580ad8dd01a62fd088b482237ea2
Rick Gallegos <duulketariakan@yahoo.com>
2017-09-09 00:54:51 -0700

    Shell commands

--- db/shell/shell.js
+++ db/shell/shell.js
@@ -1,9 +1,16 @@
-const execProcess = require('./exec_process.js');
+const shell = require('shelljs');
 
-execProcess.result(`sh ${__dirname}/temp.sh`, (err, response) => {
-  if (!err) {
-    console.log(response);
-  } else {
-    console.log(err);
-  }
-});
+// console.log('This is the shell constant', shell);
+// shell.exec('kill -l');
+// return new Promise(resolve, reject) {
+//
+//   resolve();
+// }
+console.log('DIRNAME', __dirname);
+
+const mongo = shell.exec('psql -c "\copy staging_sfs (incident_num, category, date, time, address, x, y, location) FROM `C:/Users/duulketariakan/ssp-one/my-projects/WalkSafe-server/db/CSV\staging-sf.csv` DELIMITER ',' CSV HEADER;"');
+
+// var mongo = require('child_process').spawn('psql -U postgres');
+
+// mongo.kill('SIGINT');
+// shell.exec('mysql -u root -p');
